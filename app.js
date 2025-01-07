8010fc7fa578315cc666c2a32bb51ddd540c32ce
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-07 17:15:07 +0100

    [update] app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const router express.Router();
 const postController = requuire('./controllers/postController')
+
+router.get('/posts', postController.getAllPosts);
+
+router.get('/posts/:id', postController.getPostById);
+
+router.delete('/posts/:id', postController.deletePostById);
+
+module.exports = router;
