bd71a082ffb74b8e455b85d0ca4f3f355b8e83e4
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-07 17:22:32 +0100

    [update] app.js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express = require('express');
-const router express.Router();
-const postController = requuire('./controllers/postController')
+const express = require("express");
+const router = express.Router();
+const postController = require("./controllers/postController");
 
-router.get('/posts', postController.getAllPosts);
+router.get("/posts", postController.getAllPosts);
 
-router.get('/posts/:id', postController.getPostById);
+router.get("/posts/:id", postController.getPostById);
 
-router.delete('/posts/:id', postController.deletePostById);
+router.delete("/posts/:id", postController.deletePostById);
+
+//Avvio del server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server avviato sulla porta ${PORT}`);
+});
 
 module.exports = router;
