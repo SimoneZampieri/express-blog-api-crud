0e81b4d4edeb372bfe88724a2c22912052b267ed
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-07 16:37:12 +0100

    [update] controllers

---
+++ controllers/postController.js
@@ -0,0 +1,12 @@
+// controllers/postController.js
+exports.getAllPosts = (req, res) => {
+    res.send("Restituisce tutti i post");
+};
+
+exports.getPostById = (req, res) => {
+    res.send(`Restituisce il post con ID ${req.params.id}`);
+};
+
+exports.deletePostById = (req, res) => {
+    res.status(204).send();
+};
