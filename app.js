1298aa2081c52f6388fe3dabe6d8ae879b941fee
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-08 16:34:17 +0100

    [update] milestone 4 completed

--- app.js
+++ app.js
@@ -1,38 +1,49 @@
 const express = require("express");
-const postRouter = require('./postRouter');
+const postRouter = require("./postRouter");
 const app = express();
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 const port = 3000;
 
 let posts = [];
 
-app.get('/', (req, res) => {
-    res.send('Lista post')
+app.get("/", (req, res) => {
+  res.send("Lista post");
 });
 
-app.use('/posts', postRouter);
+app.use("/posts", postRouter);
 
-app.listen(port, () =>{
-    console.log(`Sono in ascolto sulla porta ${port}`)
+app.listen(port, () => {
+  console.log(`Sono in ascolto sulla porta ${port}`);
 });
 
-app.post('/store', (req, res) => {
-    const newPost = {
-        id: posts.length + 1,
-        title: req.body.title,
-        content: req.body.content,
-        images: req.body.images
+app.post("/store", (req, res) => {
+  const newPost = {
+    id: posts.length + 1,
+    title: req.body.title,
+    content: req.body.content,
+    images: req.body.images,
+  };
+  posts.push(newPost);
+  console.log("Nuovo post aggiunto:", newPost);
+  res.status(201).send(newPost);
+});
+
+app.put("/store/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+  const postIndex = posts.findIndex((post) => post.id === postId);
+
+  if (postIndex !== -1) {
+    posts[postIndex] = {
+      id: postId,
+      title: req.body.title,
+      content: req.body.content,
+      images: req.body.images
     };
-    posts.push(newPost);
-    console.log('Nuovo post aggiunto:', newPost);
-    res.status(201).send(newPost);
+    console.log("Post aggiornato:", posts[postIndex]);
+    res.status(201).send(posts[postIndex]);
+  } else {
+    res.status(404).send({ message: "Post non trovato" });
+  };
 });
-
-app.put('/store/:id', (req, res) =>{
-    const postId = parseInt(req.params.id);
-    const postIndex = posts.findIndex(post => post.id === postId);
-
-    
-})
